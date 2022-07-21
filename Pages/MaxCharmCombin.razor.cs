9fe0c9401b9662e9b1112885d1c325cdd761d2da
dtlnor <dtlnor@hotmail.com>
2022-07-21 18:18:20 +0900

    bug fix for too much combinations

--- Pages/MaxCharmCombin.razor.cs
+++ Pages/MaxCharmCombin.razor.cs
@@ -12,19 +12,21 @@ namespace dtlnorUtilSite.Pages
     public partial class MaxCharmCombin
     {
         private JObject skillNameIDPair;
-        private SkillMax[] skillMax;
-        private SlotMax[] slotMax;
-        private DecoBaseData[] decoBaseData;
+        private SkillMax[] skillMaxs;
+        private SlotMax[] slotMaxs;
+        private DecoBaseData[] decoBaseDatas;
+        private SkillBaseData[] skillBaseDatas;
         private Dictionary<int, string> nameLookUp;
 
         protected List<string> SelectedIds = new List<string>();
 
         public string OutPutValue { get; set; }
-        public string CharmCombinText { get; set; }
+        public List<string> CharmCombinText { get; set; } = new List<string>();
+        public string CharmCount { get; set; }
 
         protected void ShowSelectedValues()
         {
-            OutPutValue = string.Join(",", SelectedIds.ToArray());
+            OutPutValue = string.Join(", ", SelectedIds.ToArray());
             StateHasChanged();
         }
         static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
@@ -40,41 +42,100 @@ namespace dtlnorUtilSite.Pages
         {
             OutPutValue = string.Join(",", SelectedIds.Select(x => nameLookUp[int.Parse(x)]).ToList());
 
+            
+            IEnumerable<IEnumerable<int>> combins = GetPermutations(SelectedIds.Select(int.Parse).ToList(), 2);
+            // construct strings
+            //string result = string.Empty;
+            int charmCount = 0;
+
+            var sb = new System.Text.StringBuilder();
+            var stringChunk = new List<string>();
+
+            foreach (var skillPair in combins)
+            {
+                var skill1 = skillMaxs.Single(skill => skill.SkillID == skillPair.First());
+                var skill2 = skillMaxs.Single(skill => skill.SkillID == skillPair.Last());
+                var slotMaxCombine = slotMaxs.Single(gradeType => gradeType.Grade == skill1.Grade + skill2.Grade);
+
+                string skillPart = string.Join(",", new List<string>
+                {
+                    nameLookUp[skill1.SkillID],
+                    skill1.Skill1Max.ToString(),
+                    nameLookUp[skill2.SkillID],
+                    skill2.Skill2Max.ToString()});
+
+                sb.AppendLine(string.Join(",", new List<string>
+                {
+                    skillPart,
+                    slotMaxCombine.Slot1Lv.ToString(),
+                    slotMaxCombine.Slot2Lv.ToString(),
+                    slotMaxCombine.Slot3Lv.ToString()
+                }));
+                charmCount++;
+                if (charmCount % 2500 == 0 && charmCount > 0)
+                {
+                    stringChunk.Add(sb.ToString());
+                    sb.Clear();
+                }
+
+                sb.AppendLine(skillPart + ",4,0,0");
+                charmCount++;
+                if (charmCount % 2500 == 0 && charmCount > 0)
+                {
+                    stringChunk.Add(sb.ToString());
+                    sb.Clear();
+                }
+            }
+            if (sb.Length > 0)
+            {
+                stringChunk.Add(sb.ToString());
+                sb.Clear();
+            }
+
+            CharmCount = charmCount.ToString();
+            CharmCombinText = stringChunk;
+            StateHasChanged();
+        }
+        protected void ShowLimitedCharmCombins(int MaxWeight = 5, int Lv4SlotWeight = 2)
+        {
+            OutPutValue = string.Join(",", SelectedIds.Select(x => nameLookUp[int.Parse(x)]).ToList());
+
             IEnumerable<IEnumerable<int>> combins = GetPermutations(SelectedIds.Select(int.Parse).ToList(), 2);
             // construct strings
             string result = string.Empty;
             foreach (var skillPair in combins)
             {
-                var SkillMax1 = skillMax.Single(skill => skill.SkillID == skillPair.First());
-                var SkillMax2 = skillMax.Single(skill => skill.SkillID == skillPair.Last());
-                var SlotMaxCombine = slotMax.Single(gradeType => gradeType.Grade == SkillMax1.Grade + SkillMax2.Grade);
-                string skillPart = string.Join(",", new List<string>
-                {
-                    nameLookUp[SkillMax1.SkillID],
-                    SkillMax1.Skill1Max.ToString(),
-                    nameLookUp[SkillMax2.SkillID],
-                    SkillMax2.Skill2Max.ToString()});
+                var skill1 = skillMaxs.Single(skill => skill.SkillID == skillPair.First());
+                var skill2 = skillMaxs.Single(skill => skill.SkillID == skillPair.Last());
+                var slotMaxCombine = slotMaxs.Single(gradeType => gradeType.Grade == skill1.Grade + skill2.Grade);
+                var skill1Deco = decoBaseDatas.Single(deco => (deco.SkillID == skill1.SkillID) && (deco.SkillLv == 1));
+                var skill2Deco = decoBaseDatas.Single(deco => (deco.SkillID == skill2.SkillID) && (deco.SkillLv == 1));
+                int skill1MaxWeight = (skill1Deco.DecoLv >= 2) ? skill1.Skill1Max : 0;
+                bool skill1isLv4Deco = skill1Deco.DecoLv == 4;
+                int skill2MaxWeight = (skill2Deco.DecoLv >= 2) ? skill2.Skill2Max : 0;
+                bool skill2isLv4Deco = skill2Deco.DecoLv == 4;
+                int slotWeight = (slotMaxCombine.Slot1Lv >= 2 ? 1 : 0) + (slotMaxCombine.Slot2Lv >= 2 ? 1 : 0) + (slotMaxCombine.Slot3Lv >= 2 ? 1 : 0);
 
-                result += string.Join(",", new List<string>
-                {
-                    skillPart,
-                    SlotMaxCombine.Slot1Lv.ToString(),
-                    SlotMaxCombine.Slot2Lv.ToString(),
-                    SlotMaxCombine.Slot3Lv.ToString() + "\n"
-                });
-
-                result += skillPart + ",4,0,0\n";
+                // for Lv3 Slot
+                int Lv3SkillTotalWeight = MaxWeight - slotWeight;
+                
             }
-            CharmCombinText = result;
+            //CharmCombinText = result;
             StateHasChanged();
+        }
+        protected List<List<int>> GetLimitRateCombination(int skill1Max, int Skill2Max, int SkillTotalMax)
+        {
+            
+            return null;
         }
 
         protected override async Task OnInitializedAsync()
         {
             skillNameIDPair = JObject.Parse((await Http.GetFromJsonAsync<System.Text.Json.JsonElement>("skill-data/SKILL_NAME_LOOKUP.json")).GetRawText());
-            skillMax = await Http.GetFromJsonAsync<SkillMax[]>("skill-data/SkillMax.json");
-            slotMax = await Http.GetFromJsonAsync<SlotMax[]>("skill-data/SlotMax.json");
-            decoBaseData = await Http.GetFromJsonAsync<DecoBaseData[]>("skill-data/DecoBaseData.json");
+            skillMaxs = await Http.GetFromJsonAsync<SkillMax[]>("skill-data/SkillMax.json");
+            slotMaxs = await Http.GetFromJsonAsync<SlotMax[]>("skill-data/SlotMax.json");
+            decoBaseDatas = await Http.GetFromJsonAsync<DecoBaseData[]>("skill-data/DecoBaseData.json");
+            skillBaseDatas = await Http.GetFromJsonAsync<SkillBaseData[]>("skill-data/SkillBaseData.json");
         }
 
         public class SkillMax
@@ -83,6 +144,14 @@ namespace dtlnorUtilSite.Pages
             public string Grade { get; set; }
             public int Skill1Max { get; set; }
             public int Skill2Max { get; set; }
+        }
+
+        public class SkillBaseData
+        {
+            public int SkillID { get; set; }
+            public int MaxLevel { get; set; }
+            public int IconColor { get; set; }
+            public int WorthPointList { get; set; }
         }
 
         public class SlotMax

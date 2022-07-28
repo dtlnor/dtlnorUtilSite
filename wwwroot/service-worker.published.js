01723cb3bef03ded6f123489ddf2c6ce25044c69
dtlnor <dtlnor@hotmail.com>
2022-07-28 16:59:20 +0900

    Update service-worker.published.js

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -27,14 +27,15 @@ const notifyNewVersion = () => {
 async function onInstall(event) {
     console.info('Service worker: Install');
 
+    // Activate the new service worker as soon as the old one is retired.
+    self.skipWaiting();
+
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url, { integrity: asset.hash }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
-
-    notifyNewVersion();
 }
 
 async function onActivate(event) {
